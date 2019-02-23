0363627d7b936cd7bffd403259801981b197caf8
HBahaa <hebabahaa2208@yahoo.com>
2019-02-23 20:38:40 +0200

    some changes in dashboard

--- src/components/views/Dashboard.vue
+++ src/components/views/Dashboard.vue
@@ -68,74 +68,29 @@
     </div>
 
     <div class="row">
-      <div class="col-xs-12">
+      <div class="col-xs-7">
         <div class="box">
           <div class="box-header with-border">
-            <h3 class="box-title"></h3>
+            <h3 class="box-title">Registrations</h3>
             <div class="box-body">
-              <div class="col-sm-6 col-xs-12">
-                <p class="text-center">
-                  <strong>Web Traffic Overview</strong>
-                </p>
-                <canvas id="trafficBar" ></canvas>
-              </div>
-              <hr class="visible-xs-block">
-              <div class="col-sm-6 col-xs-12">
-                <p class="text-center">
-                  <strong>Language Overview</strong>
-                </p>
-                <canvas id="languagePie"></canvas>
-              </div>
+              <canvas id="trafficBar1" ></canvas>
             </div>
           </div>
-          <div class="text-center">
-            <small><b>Pro Tip</b> Don't forget to star us on github!</small>
+        </div>
+      </div>
+      <div class="col-xs-5">
+        <div class="box">
+          <div class="box-header with-border">
+            <h3 class="box-title">Website Visits</h3>
+            <div class="box-body">
+                <canvas id="trafficBar2"></canvas>
+            </div>
           </div>
         </div>
       </div>
     </div>
     <!-- /.row -->
 
-    <!-- Main row -->
-    <div class="row">
-      <div class="col-md-3 col-sm-6 col-xs-12">
-        <process-info-box color-class="bg-yellow"
-                          :icon-classes="['ion', 'ion-ios-pricetag-outline']"
-                          text="Inventory"
-                          number="5,200"
-                          :progress="50"
-                          description="50% increase since May"></process-info-box>
-      </div>
-      <!-- /.col -->
-      <div class="col-md-3 col-sm-6 col-xs-12">
-        <process-info-box color-class="bg-green"
-                          :icon-classes="['ion', 'ion-ios-heart-outline']"
-                          text="Mentions"
-                          number="92,050"
-                          :progress="20"
-                          description="20% increase in 30 days"></process-info-box>
-      </div>
-      <!-- /.col -->
-      <div class="col-md-3 col-sm-6 col-xs-12">
-        <process-info-box color-class="bg-red"
-                          :icon-classes="['ion', 'ion-ios-cloud-download-outline']"
-                          text="Downloads"
-                          number="114,381"
-                          :progress="70"
-                          description="70% increase since yesterday"></process-info-box>
-      </div>
-      <!-- /.col -->
-      <div class="col-md-3 col-sm-6 col-xs-12">
-        <process-info-box color-class="bg-aqua"
-                          :icon-classes="['ion', 'ion-ios-chatbubble-outline']"
-                          text="Direct Messages"
-                          number="163,921"
-                          :progress="40"
-                          description="40% increase compared to last year"></process-info-box>
-      </div>
-      <!-- /.col -->
-    </div>
-    <!-- /.row -->
   </section>
   <!-- /.content -->
 </template>
@@ -177,8 +132,8 @@ export default {
   },
   mounted () {
     this.$nextTick(() => {
-      var ctx = document.getElementById('trafficBar').getContext('2d')
-      var config = {
+      var ctx1 = document.getElementById('trafficBar1').getContext('2d')
+      var config1 = {
         type: 'line',
         data: {
           labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
@@ -213,17 +168,26 @@ export default {
         }
       }
 
-      new Chart(ctx, config) // eslint-disable-line no-new
+      new Chart(ctx1, config1) // eslint-disable-line no-new
 
-      var pieChartCanvas = document.getElementById('languagePie').getContext('2d')
-      var pieConfig = {
-        type: 'pie',
+      var ctx2 = document.getElementById('trafficBar2').getContext('2d')
+      var config2 = {
+        type: 'line',
         data: {
-          labels: ['HTML', 'JavaScript', 'CSS'],
+          labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
           datasets: [{
-            data: [56.6, 37.7, 4.1],
-            backgroundColor: ['#00a65a', '#f39c12', '#00c0ef'],
-            hoverBackgroundColor: ['#00a65a', '#f39c12', '#00c0ef']
+            label: 'Admin Panel',
+            fill: false,
+            borderColor: '#284184',
+            pointBackgroundColor: '#284184',
+            backgroundColor: 'rgba(0, 0, 0, 0)',
+            data: this.coPilotNumbers
+          }, {
+            label: 'Personal Site',
+            borderColor: '#4BC0C0',
+            pointBackgroundColor: '#4BC0C0',
+            backgroundColor: 'rgba(0, 0, 0, 0)',
+            data: this.personalNumbers
           }]
         },
         options: {
@@ -232,11 +196,17 @@ export default {
           legend: {
             position: 'bottom',
             display: true
+          },
+          tooltips: {
+            mode: 'label',
+            xPadding: 10,
+            yPadding: 10,
+            bodySpacing: 10
           }
         }
       }
 
-      new Chart(pieChartCanvas, pieConfig) // eslint-disable-line no-new
+      new Chart(ctx2, config2) // eslint-disable-line no-new
     })
   }
 }
@@ -292,5 +262,16 @@ export default {
 }
 .c100.third-circle .bar{
   border-color: #29cc97 !important;
+}
+/* h3{
+  font-weight: 400;
+  margin-bottom: 20px;
+  margin-top: 0;
+  color: #b2b2b2;
+} */
+.box-header .box-title {
+  color: #b2b2b2;
+  font-size: 24px;
+  margin: 10px 0 20px 20px;
 }
 </style>
