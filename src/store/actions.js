f0508bb555b6ad50009db4a72ab98e8c41758db8
HBahaa <hebabahaa2208@yahoo.com>
2019-03-01 17:35:42 +0200

    login method in store and login page

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,1 +1,16 @@
-export default {}
+import axios from 'axios'
+
+let header2 = {
+  'async': true,
+  'crossDomain': true,
+  'headers': {
+    'accept': 'application/json',
+    'content-type': 'application/json'
+  }
+}
+
+export default {
+  login(vuexContext, data) {
+    return axios.post('http://pthat.nozm.net/api/auth/login', data, header2)
+  }
+}
