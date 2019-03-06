484d977b7677bb7764850506f2cbbc84618d98a0
ahmedbakheet22 <46079273+ahmedbakheet22@users.noreply.github.com>
2019-03-06 11:37:35 +0200

    a

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
-const app = express();
-
-// app.use(serveStatic(path.join(__dirname, './dist')));
-app.use('/dist', express.static(path.join(__dirname, './dist')));
-const port = process.env.PORT || 8080;
-const host = process.env.HOST || '0.0.0.0'
+app = express();
+app.use(serveStatic(path.join(__dirname, 'dist')));
+const port = process.env.PORT || 80;
+app.listen(port);
